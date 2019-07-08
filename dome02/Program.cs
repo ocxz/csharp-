3da51208c89c4987e4d7d18ceeb10d93be84714a
ocxz <2252779530@qq.com>
2019-07-08 16:02:42 +0800

    第六次提交

--- dome02/Program.cs
+++ dome02/Program.cs
@@ -7,6 +7,8 @@ namespace dome02
         static void Main(string[] args)
         {
             Console.WriteLine("This is my seconde project");
+            String zsName = "张三";
+            String s = "";
             Console.ReadKey();
         }
     }
