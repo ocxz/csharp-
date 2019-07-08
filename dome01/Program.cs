2ea0fdf00e5ef56a08b8568e1e2036df8e01d2fd
ocxz <2252779530@qq.com>
2019-07-08 13:29:40 +0800

    添加项目文件。

---
+++ dome01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dome01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
